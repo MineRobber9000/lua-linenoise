78fe06c8b92d735848c7bc99cb77dbef2a81d9bf
MineRobber9000 <khuxkm@ttm.sh>
2024-11-04 00:21:41 -0500

    remove umask calls since emscripten doesn't have umask

--- linenoiselib.c
+++ linenoiselib.c
@@ -1372,12 +1372,12 @@ int linenoiseHistorySetMaxLen(int len) {
 /* Save the history in the specified file. On success 0 is returned
  * otherwise -1 is returned. */
 int linenoiseHistorySave(const char *filename) {
-    mode_t old_umask = umask(S_IXUSR|S_IRWXG|S_IRWXO);
+//    mode_t old_umask = umask(S_IXUSR|S_IRWXG|S_IRWXO);
     FILE *fp;
     int j;
 
     fp = fopen(filename,"w");
-    umask(old_umask);
+//    umask(old_umask);
     if (fp == NULL) return -1;
     chmod(filename,S_IRUSR|S_IWUSR);
     for (j = 0; j < history_len; j++)
